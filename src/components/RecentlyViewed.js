b570d574654539f70e07daefdb5684293094c38b
Ahmad <ahmadullah943@gmail.com>
2024-06-04 01:29:46 +0500

    feat: recently viewed photos

--- src/components/RecentlyViewed.js
+++ src/components/RecentlyViewed.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import Photo from './Photo';
-import { Grid, Container, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Container, Typography, Grid, Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import axios from 'axios';
 
-const RecentlyViewed = ({ photos }) => {
+const RecentlyViewed = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/photos?_limit=20');
+        setPhotos(response.data);
+      } catch (error) {
+        console.error('Error fetching photos:', error);
+      }
+    };
+
+    fetchPhotos();
+  }, []);
+
   return (
     <Container>
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h4" gutterBottom paddingTop={"15px"}>
         Recently Viewed Photos
       </Typography>
       <Grid container spacing={2}>
         {photos.map((photo) => (
           <Grid item key={photo.id} xs={12} sm={6} md={4}>
-            <Photo photo={photo} onClick={() => {}} />
+            <Card sx={{ maxWidth: 345, margin: 2 }}>
+              <CardActionArea>
+                <CardMedia component="img" height="140" image={photo.thumbnailUrl} alt={photo.title} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {photo.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           </Grid>
         ))}
       </Grid>
